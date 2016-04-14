48ffeca8cdcb89d165318411ff4467ba4df81c51
ssugar <scottsugar@outlook.com>
2016-04-14 22:09:03 +0700

    moving back to /var/www/html now that we are using an installer script post container start

--- ccvpress/config_wordpress.sh
+++ ccvpress/config_wordpress.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-DIR="/var/www2/html"
+DIR="/var/www/html"
 if [ "$(ls -A $DIR)" ] ; then
     echo "Looks like $DIR is not empty, not installing"
 else
-    cp -R /usr/src/wordpress/* /var/www2/html && \
-      chown -R www-data:www-data /var/www2/html && \
-	  sudo -u www-data -- wp core config --dbname=wordpress --dbuser=root --dbpass=insecurepass --dbhost=mysql --path=/var/www2/html && \
-      sudo -u www-data -- wp db create --path=/var/www2/html && \
-      sudo -u www-data -- wp core install --url=http://10.10.10.56 --title=ccv --admin_user=ccvAdmin --admin_password=insecurepass --admin_email=info@canchamvietnam.org --path=/var/www2/html && \
+    cp -R /usr/src/wordpress/* $DIR && \
+      chown -R www-data:www-data $DIR && \
+	  sudo -u www-data -- wp core config --dbname=wordpress --dbuser=root --dbpass=insecurepass --dbhost=mysql --path=$DIR && \
+      sudo -u www-data -- wp db create --path=$DIR && \
+      sudo -u www-data -- wp core install --url=http://10.10.10.56 --title=ccv --admin_user=ccvAdmin --admin_password=insecurepass --admin_email=info@canchamvietnam.org --path=$DIR && \
 	  wget https://download.civicrm.org/civicrm-4.7.6-wordpress.zip && \
-	  mkdir -p /var/www2/html/wp-content/plugins  && \
-	  mv civicrm-4.7.6-wordpress.zip /var/www2/html/wp-content/plugins/civicrm-4.7.6-wordpress.zip  && \
-	  unzip /var/www2/html/wp-content/plugins/civicrm-4.7.6-wordpress.zip -d /var/www2/html/wp-content/plugins
+	  mkdir -p $DIR/wp-content/plugins  && \
+	  mv civicrm-4.7.6-wordpress.zip $DIR/wp-content/plugins/civicrm-4.7.6-wordpress.zip  && \
+	  unzip $DIR/wp-content/plugins/civicrm-4.7.6-wordpress.zip -d $DIR/wp-content/plugins
 fi
