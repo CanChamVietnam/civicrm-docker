2a11408190584a6e77b06d5aae6db14d91c65a95
ssugar <scottsugar@outlook.com>
2016-04-14 22:55:24 +0700

    adding variables

--- ccvpress/config_wordpress.sh
+++ ccvpress/config_wordpress.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 DIR="/var/www/html"
+DBPASS="insecurepass"
+SITEPASS="insecurepass"
+SITE="http://10.10.10.56"
+
 if [ "$(ls -A $DIR)" ] ; then
     echo "Looks like $DIR is not empty, not installing"
 else
     cp -R /usr/src/wordpress/* $DIR && \
       chown -R www-data:www-data $DIR && \
-	  sudo -u www-data -- wp core config --dbname=wordpress --dbuser=root --dbpass=insecurepass --dbhost=mysql --path=$DIR && \
+	  sudo -u www-data -- wp core config --dbname=wordpress --dbuser=root --dbpass=$DBPASS --dbhost=mysql --path=$DIR && \
       sudo -u www-data -- wp db create --path=$DIR && \
-      sudo -u www-data -- wp core install --url=http://10.10.10.56 --title=ccv --admin_user=ccvAdmin --admin_password=insecurepass --admin_email=info@canchamvietnam.org --path=$DIR && \
+      sudo -u www-data -- wp core install --url=$SITE --title=ccv --admin_user=ccvAdmin --admin_password=$SITEPASS --admin_email=info@canchamvietnam.org --path=$DIR && \
 	  wget https://download.civicrm.org/civicrm-4.7.6-wordpress.zip && \
 	  mkdir -p $DIR/wp-content/plugins  && \
 	  mv civicrm-4.7.6-wordpress.zip $DIR/wp-content/plugins/civicrm-4.7.6-wordpress.zip  && \
